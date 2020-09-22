29b90fcc69766696449ccc5617e7f5412519b7f9
Meredith McIntosh <meredith@mcintosh.io>
2020-09-22 19:12:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,4 +9,4 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-end happy_birthday(birthday_kids)
+end happy_birthday
