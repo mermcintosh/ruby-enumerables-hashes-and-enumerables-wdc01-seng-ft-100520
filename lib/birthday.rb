26cb52fc4c55621861019cfdfa84ca881f3a9da5
Meredith McIntosh <meredith@mcintosh.io>
2020-09-22 18:55:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,12 +4,6 @@
 #   "Amanda" => 27
 # }
 
-# def happy_birthday(birthday_kids)
-#   # add your code snippet here!
-# end
-
-hash = {key1: "value1", key2: "value2"}
- 
-hash.each do |key, value|
-  puts "#{key}: #{value}"
+def happy_birthday(birthday_kids)
+  # add your code snippet here!
 end
